f7f5668d493d9bb8c6258fa0b4eadd2fb78b4c28
Gabriel Evangelista <gabriel.evan.queiroz@gmail.com>
2018-03-16 12:04:20 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = mysqli_connect("localhost", "root", "fiap", "loginapp");
+  $conn = mysqli_connect("localhost", "root", "PASSWORD", "loginapp");
   if(!$conn){
     die('Não deu certo a conexão ' . mysqli_error());
   } 
