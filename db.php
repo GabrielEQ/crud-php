b80fe7cc9bc6d164eac5126e4657982cae106b35
Gabriel Evangelista <gabriel.evan.queiroz@gmail.com>
2018-04-06 01:30:16 -0300

    testes

--- db.php
+++ db.php
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
 <?php
-  $conn = mysqli_connect("localhost", "root", "fiap", "loginapp");
+  $conn = mysqli_connect("localhost", "root", "142184Biel", "loginapp");
   if(!$conn){
     die('Não deu certo a conexão ' . mysqli_error());
   } 
 
 ?>
-=======
-<?php
-  $conn = mysqli_connect("localhost", "root", "fiap", "loginapp");
-  if(!$conn){
-    die('Não deu certo a conexão ' . mysqli_error());
-  } 
-
-?>
->>>>>>> 440bd65b403b88738c487582777013abe3ee1669
--- login_read.php
+++ login_read.php
@@ -1,54 +1,13 @@
-<<<<<<< HEAD
 <?php
-    $conn = mysqli_connect('localhost', 'root', '142184Biel', 'loginapp');
-    if(!$conn){
-        die('Deu ruim!');
-    }
-
-    $query = "SELECT * FROM cadastro";
-    $result = mysqli_query($conn, $query);
-
-?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="utf-8"/>
-        <title>PHP</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"/>
-    </head>
-
-    <body>
-      <div class="container">
-        <div class="col-sm-6">
-         <?php
-            while($row = mysqli_fetch_assoc($result)){
-                // print_r($row);
-                $id = $row['id'];
-                $username = $row['username'];
-                $password = $row['password'];
-
-                echo "<pre>" . $id;
-                echo " ". $username;
-                echo " ". $password. "</pre>";
-            }
-         ?>
-        </div>
-      </div>
-    </body>
-</html>
-=======
-<?php 
     include "db.php";
     include "functions.php";
 ?>
 <?php include "header.php"; ?>
       <div class="container">
         <div class="col-sm-6">
-        <?php 
+        <?php
             lerTabela();
         ?>
         <?php include "buttons.php"; ?>
         </div>
 <?php include "footer.php"; ?>
->>>>>>> 440bd65b403b88738c487582777013abe3ee1669
